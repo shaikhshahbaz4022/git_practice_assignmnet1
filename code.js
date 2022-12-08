febb758743bc32bbc29ff9b0d78040fccb9aa353
unknown <shaikhshahbaz4022@gmail.com>
2022-12-08 23:03:46 +0530

    Now the code is Fixed

--- code.js
+++ code.js
@@ -6,18 +6,18 @@ let num=13;
 
 for(let i=0;i<num;i++)
 {
-	if(num%i==0)
-		{
-			count++
-		}
+if(num%i==0)
+{
+	count++;
+}
 }
 if(count==2)
 {
-console.log(num, "No. is prime")
+console.log(num, "No. is prime");
 }
 else
 {
-console.log(i, "No. is not prime")
+console.log(i, "No. is not prime");
 }
 
 *****************************XXX*************************************
